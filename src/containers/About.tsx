b7a160360ef06df6941d93935f561bd5ff7ca964
Alexander <AlexMC@Macs-MacBook-Pro.local>
2019-03-18 17:09:47 +0200

    redux

--- src/containers/About.tsx
+++ src/containers/About.tsx
@@ -1,13 +1,48 @@
 import * as React from "react";
+import styled from "styled-components";
+import { connect } from "react-redux";
+import { Dispatch } from "redux";
 
-export interface AboutProps {}
+const PageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
-const About: React.SFC<AboutProps> = () => {
+const AboutContainer = styled.div`
+  max-width: 1024px;
+  min-width: 600px;
+`;
+
+export interface AboutProps {
+  counter: number;
+  increment: () => void;
+  decrement: () => void;
+}
+
+const About: React.SFC<AboutProps> = ({ counter, increment, decrement }) => {
   return (
-    <div>
-      <h1>About Us</h1>
-    </div>
+    <PageContainer>
+      <AboutContainer>
+        <h1>About Us</h1>
+        Counter: {counter}
+        <hr />
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+      </AboutContainer>
+    </PageContainer>
   );
 };
 
-export default About;
+const mapStateToProps = (state: number) => ({
+  counter: state
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  increment: () => dispatch({ type: "INCREMENT" }),
+  decrement: () => dispatch({ type: "DECREMENT" })
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(About);
