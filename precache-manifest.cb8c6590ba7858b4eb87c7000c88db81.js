dd0b1a8da5a7df6030f743201b66da10cc4207bd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-13 23:33:46 +0000

    Deploy xitman78/react-minesweeper to github.com/xitman78/react-minesweeper.git:gh-pages

---
+++ precache-manifest.cb8c6590ba7858b4eb87c7000c88db81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbdd297631b0c5602203",
+    "url": "/react-minesweeper/static/css/main.a970dc24.chunk.css"
+  },
+  {
+    "revision": "fbdd297631b0c5602203",
+    "url": "/react-minesweeper/static/js/main.ece35a13.chunk.js"
+  },
+  {
+    "revision": "a36eadc15cff40b610bc",
+    "url": "/react-minesweeper/static/js/runtime~main.ea74b7a1.js"
+  },
+  {
+    "revision": "dd8694705a2bfc2805e4",
+    "url": "/react-minesweeper/static/js/2.a9231b34.chunk.js"
+  },
+  {
+    "revision": "83ece4b8c9e68f928fab7d8f8dd5f086",
+    "url": "/react-minesweeper/index.html"
+  }
+];
